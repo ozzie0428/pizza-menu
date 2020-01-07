ccde331ab54aa68e5737c054c0f7432c0be7605a
DenimCity <altidorj1@gmail.com>
2020-01-06 19:11:35 -0500

    add description

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import SplashImage from './components/SplashImage'
+import Description from './components/Description';
 class App extends Component {
   render() {
     return (
@@ -8,6 +9,7 @@ class App extends Component {
        
       <Navbar/>
       <SplashImage/>
+      <Description/>
       </div>
 
     );
